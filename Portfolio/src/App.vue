65cb1d8d189e150736ad316c005a547fefebb144
Beatriz <beatriz.sfrodrigues@gmail.com>
2023-10-15 03:17:35 +0200

    aii

---
+++ Portfolio/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+// import Border from "./components/Border.vue";
+import Home from "./views/HomeView.vue";
+</script>
+
+<template>
+  <!-- <Border /> -->
+  <Home />
+</template>
+
+<style>
+@import "./assets/base.css";
+</style>
---
+++ Portfolio/src/views/HomeView.vue
@@ -0,0 +1,65 @@
+<script setup></script>
+
+<template>
+  <div id="border">
+    <div id="left">
+      <div class="top">
+        <div class="borderBox">
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/005/544/770/small/profile-icon-design-free-vector.jpg"
+            id="profilePic"
+          />
+        </div>
+        <div id="info">
+          <h2 id="name">BEATRIZ RODRIGUES</h2>
+          <!-- <hr /> -->
+          <div id="topBottom">
+            <ul>
+              <li>Student at ESMAD</li>
+              <li>TSIW degree</li>
+              <li>19 years old</li>
+            </ul>
+          </div>
+          <!-- <hr id="hrBottom" /> -->
+        </div>
+      </div>
+      <p>
+        I'm a 19 year old college student studying web programming and design.
+        I'm in my third year of college in the Undergraduate Degree in Web
+        Information Systems and Technologies course at ESMAD.
+      </p>
+      <div id="socialDiv">
+        <img
+          src="https://static-00.iconduck.com/assets.00/logo-social-media-tiktok-icon-256x256-0vdcnwtl.png"
+          alt=""
+          class="socialMedia"
+        />
+        <img
+          src="https://static-00.iconduck.com/assets.00/logo-social-media-tiktok-icon-256x256-0vdcnwtl.png"
+          alt=""
+          class="socialMedia"
+        />
+        <img
+          src="https://static-00.iconduck.com/assets.00/logo-social-media-tiktok-icon-256x256-0vdcnwtl.png"
+          alt=""
+          class="socialMedia"
+        />
+      </div>
+    </div>
+
+    <div id="right">
+      <div id="divExplore">
+        <h2 id="explore">EXPLORE</h2>
+        <div id="exploreButtons">
+          <button>Projects</button>
+          <button>About Me</button>
+          <button>Contacts</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@import "../assets/home.css";
+</style>
